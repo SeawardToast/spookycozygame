499b5f608ea7dab772948251af264f6a3a9cc824
aidan <aidan@murphyhouse.us>
2026-01-12 14:59:00 -0500

    fixing scene / game manager stuff

--- scripts/globals/game_manager.gd
+++ scripts/globals/game_manager.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var game_menu_screen: Resource = preload("res://scenes/ui/game_menu_screen.tscn")
+var main_scene_path: String = "res://scenes/main_scene.tscn"
+var main_scene_root_path: String = "/root/MainScene"
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("game_menu"):
@@ -8,9 +10,15 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func start_game() -> void:
-	print("game started")
-	SceneManager.load_main_scene_container()
-	SceneManager.load_level("level1") 
+	# load main scene
+	if get_tree().root.has_node(main_scene_root_path):
+		return
+	
+	var node: Node = load(main_scene_path).instantiate()
+	
+	if node != null:
+		get_tree().root.add_child(node)
+
 	SaveGameManager.load_game()
 	SaveGameManager.allow_save_game = true
 
