0a7be664fc1157f3af9dc8e1ee93d3ecae6093bc
aidan <aidan@murphyhouse.us>
2026-03-16 17:46:09 -0400

    alert layout fixed

--- scenes/ui/hotel_management_ui/alert.gd
+++ scenes/ui/hotel_management_ui/alert.gd
@@ -1,16 +1,26 @@
 class_name AlertBlock
 extends Control
 
-@onready var _label: Label = $HBoxContainer/Label
-@onready var _dismiss_button: Button = $HBoxContainer/DismissButton
-
+@onready var _dismiss_button: Button = $MarginContainer/HBoxContainer/DismissButton
+@onready var _vbox: VBoxContainer = $MarginContainer/HBoxContainer/VBoxContainer
 
 func _ready() -> void:
 	_dismiss_button.pressed.connect(_on_dismiss_pressed)
 
 
 func setup(message: String) -> void:
-	_label.text = message
+	for child in _vbox.get_children():
+		child.queue_free()
+
+	var lines := message.split("\n")
+	var multiline := lines.size() > 1
+	_vbox.alignment = BoxContainer.ALIGNMENT_CENTER if not multiline else BoxContainer.ALIGNMENT_BEGIN
+	for line: String in lines:
+		var label := Label.new()
+		label.theme_type_variation = &"DayAndNightLabel"
+		label.text = line
+		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER if not multiline else HORIZONTAL_ALIGNMENT_LEFT
+		_vbox.add_child(label)
 
 
 func _on_dismiss_pressed() -> void:
